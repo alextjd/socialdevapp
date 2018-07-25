3c1e16867c72e861054b235489a06c121f36a503
alextjd <alextjd1@gmail.com>
2018-07-25 15:50:22 +0200

    Basic DB connection setup.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// File to store the keys of the app
+// Make the info available from other files
+module.exports = {
+  mongoURI: "mongodb://alex:alex123@ds247171.mlab.com:47171/socialdevapp"
+};
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-// Import express
 const express = require("express");
+const moongoose = require("mongoose"); // Communication interface with MongoDB
+
 // Initialize espress
 const app = express();
 
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect with MongoDB
+moongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 // Method to call when requests to '/' are received
-app.get("/", () => {
-  res.send("Hello");
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+// Set the port. First element for Heroku
+const port = process.env.PORT || 5000;
+
+// Set the app to run on the port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
