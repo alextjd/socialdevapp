db4aa90fa7453c87d49d9e3c9b37aeea14cce648
alextjd <alextjd1@gmail.com>
2018-07-25 16:20:03 +0200

    Correctly commenting the API routes.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route     GET /api/users/test
+// @desc      Tests the post route
+// @access    Private
 router.get("/test", (req, res) => {
   res.json({ msg: "All went fine in USERS." });
 });
