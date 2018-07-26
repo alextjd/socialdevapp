3338a4123be93fd9d462ae4e05e3be00aade7625
alextjd <alextjd1@gmail.com>
2018-07-26 12:31:19 +0200

    Added a secret key for the token.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // File to store the keys of the app
 // Make the info available from other files
 module.exports = {
-  mongoURI: "mongodb://alex:alex123@ds247171.mlab.com:47171/socialdevapp"
+  mongoURI: "mongodb://alex:alex123@ds247171.mlab.com:47171/socialdevapp",
+  secret: "mysecretandhighlysecuresecretkey"
 };
