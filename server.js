c5cf4285db4205c557c71234fad96723ced217e2
alextjd <alextjd1@gmail.com>
2018-07-26 13:58:27 +0200

    Configuring the passport middleware.

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const moongoose = require("mongoose"); // Communication interface with MongoDB
 const bodyParser = require("body-parser"); // Necessary to read HTTP request data
+const passport = require("passport");
 
+// Import the application modules
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 // Initialize espress
 const app = express();
-
-// Setup the app to be able to read HTTP request data
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // DB config
 const db = require("./config/keys").mongoURI;
+
+// BodyParser middleware to read HTTP request data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Connect with MongoDB
 moongoose
@@ -26,10 +27,10 @@ moongoose
     console.log(err);
   });
 
-// Method to call when requests to '/' are received
-app.get("/", (req, res) => {
-  res.send("Hello there");
-});
+// Passport middleware for authentication
+app.use(passport.initialize());
+// Passport configuration
+require("./config/passport")(passport);
 
 // User routes
 app.use("/api/users", users);
@@ -38,7 +39,7 @@ app.use("/api/profile", profile);
 // Posts routes
 app.use("/api/posts", posts);
 
-// Set the port. First element for Heroku
+// Set the port; first element for Heroku
 const port = process.env.PORT || 5000;
 
 // Set the app to run on the port
