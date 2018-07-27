0dd990f380c6f20b0cdac383ae9a2e6c4650389e
alextjd <alextjd1@gmail.com>
2018-07-27 13:57:22 +0200

    Additional pwd validation.

--- validation/validation.js
+++ validation/validation.js
@@ -4,6 +4,8 @@ const Validator = require("validator");
 const isEmpty = value => {
     if (typeof value === "string") {
         return value.length === 0;
+    } else if (value === undefined || value === null) {
+        return true;
     } else if (Array.isArray(value) === true && value !== null) {
         return value.length === 0;
     } else if (typeof value === "object" && value !== null) {
@@ -16,14 +18,28 @@ const isEmpty = value => {
 const validateRegister = input => {
     let errors = {};
 
-    if (!Validator.isLength(DataCue.name, { min: 3, max: 17 })) {
-        errors.name = "Name must be between 3 and 17 characters long";
-    }
-    if (!Validator.isEmail(DataCue.email)) {
-        errors.name = "Please choose a valid email";
-    }
-    if (!Validator.isLength(DataCue.pwd, { min: 6 })) {
-        errors.pwd = "Password must be at least 6 characters long";
+    if (
+        isEmpty(input.name) ||
+        isEmpty(input.email) ||
+        isEmpty(input.pwd) ||
+        isEmpty(input.repeat_pwd)
+    ) {
+        errors.empty = "No field can be empty";
+    } else {
+        if (!Validator.isLength(input.name, { min: 3, max: 17 })) {
+            errors.name = "Name must be between 3 and 17 characters long";
+        }
+        if (!Validator.isEmail(input.email)) {
+            errors.name = "Please choose a valid email";
+        }
+        if (
+            !Validator.isLength(input.pwd, { min: 6 }) ||
+            !Validator.isLength(input.repeat_pwd, { min: 6 })
+        ) {
+            errors.pwd = "Password must be at least 6 characters long";
+        } else if (input.pwd !== input.repeat_pwd) {
+            errors.pwd = "Passwords must match";
+        }
     }
 
     return {
