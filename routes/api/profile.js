ea32549feff9c94994a30d01816d98cc6141c368
alextjd <alextjd1@gmail.com>
2018-07-27 20:04:55 +0200

    Post method for profile. Creates a new one or updates the existing one.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,4 +39,76 @@ router.get(
     }
 );
 
+// @route     POST /api/profile
+// @desc      Create a profile for the token's user
+// @access    Private
+router.post(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        // Prepare the profile values
+        const profileData = {};
+        profileData.user = req.user.id;
+        if (req.body.handle) profileData.handle = req.body.handle;
+        if (req.body.company) profileData.company = req.body.company;
+        if (req.body.website) profileData.website = req.body.website;
+        if (req.body.location) profileData.location = req.body.location;
+        if (req.body.status) profileData.status = req.body.status;
+        // Split skills CSV into array
+        if (typeof req.body.skills !== "undefined") {
+            profileData.skills = req.body.skills.split(",");
+        }
+        if (req.body.bio) profileData.bio = req.body.bio;
+        if (req.body.githubusername)
+            profileData.githubusername = req.body.githubusername;
+        if (req.body.experience) profileData.experience = req.body.experience;
+        if (req.body.education) profileData.education = req.body.education;
+        // Set individually the social networks
+        profileData.social = {};
+        if (req.body.social.twitter)
+            profileData.social.twitter = req.body.social.twitter;
+        if (req.body.social.instagram)
+            profileData.social.instagram = req.body.social.instagram;
+
+        Profile.findOne({ user: req.user.id })
+            .then(profile => {
+                // Update the user's profile
+                if (profile) {
+                    Profile.findOneAndUpdate(
+                        { user: req.user.id },
+                        profileData,
+                        { new: true }
+                    )
+                        .then(() => {
+                            res.json(profile);
+                        })
+                        .catch(err => {
+                            res.json(err);
+                        });
+                }
+                // Create a new profile for the user
+                else {
+                    // The handle must be unique
+                    Profile.findOne({ handle: req.user.handle }).then(
+                        profile => {
+                            // Handle already taken
+                            if (profile) {
+                                res.status(400).json({
+                                    msg: "Handle already taken"
+                                });
+                            }
+                            // Save the new profile
+                            new Profile(profileData).save().then(profile => {
+                                res.json(profile);
+                            });
+                        }
+                    );
+                }
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    }
+);
+
 module.exports = router;
